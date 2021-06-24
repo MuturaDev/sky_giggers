881f5c7ccb9c004c2ca2843b09333a94f39bdb12
JamesGituma <jamesgituma9961@gmail.com>
2021-06-24 09:42:46 +0300

    first commit sky gigger

---
+++ app/src/main/java/com/sky/giggers/widgets/WebLayout.java
@@ -0,0 +1,50 @@
+package com.sky.giggers.widgets;
+
+
+import android.app.Activity;
+
+import com.just.agentweb.IWebLayout;
+
+import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
+import com.sky.giggers.R;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Created by cenxiaozhong on 2017/7/1.
+ * source code  https://github.com/Justson/AgentWeb
+ */
+
+public class WebLayout implements IWebLayout {
+
+    private Activity mActivity;
+    private final TwinklingRefreshLayout mTwinklingRefreshLayout;
+    private WebView mWebView = null;
+
+    public WebLayout(Activity activity) {
+        this.mActivity = activity;
+        mTwinklingRefreshLayout = (TwinklingRefreshLayout) LayoutInflater.from(activity).inflate(R.layout.fragment_twk_web, null);
+        mTwinklingRefreshLayout.setPureScrollModeOn();
+        mWebView = (WebView) mTwinklingRefreshLayout.findViewById(R.id.webView);
+    }
+
+    @NonNull
+    @Override
+    public ViewGroup getLayout() {
+        return mTwinklingRefreshLayout;
+    }
+
+    @Nullable
+    @Override
+    public WebView getWebView() {
+        return mWebView;
+    }
+
+
+
+}
